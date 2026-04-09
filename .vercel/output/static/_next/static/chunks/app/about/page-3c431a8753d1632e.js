512cae6eab327f038ca5bd2c20af68d111a13133
Darrin Mish <dmishesq@getirshelp.com>
2026-04-09 16:37:09 -0400

    Remove build artifacts from repo

--- .vercel/output/static/_next/static/chunks/app/about/page-3c431a8753d1632e.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[301,931,799,961,457,827,369],{3521:function(){}},function(n){n.O(0,[971,117,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
